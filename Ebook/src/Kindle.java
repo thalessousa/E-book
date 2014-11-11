10d114fca112f911d9e172eee77b75a8b282e5fa
thalessousa <thales_cross@hotmail.com>
2014-11-11 13:29:49 -0300

    Classes para implementar

---
+++ Ebook/src/Kindle.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LAB4-PC09
+ */
+public class Kindle extends Reader{
+
+    @Override
+    public void ler(){}
+}
---
+++ Ebook/src/Reader.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LAB4-PC09
+ */
+public abstract class Reader implements Tela {
+    
+    
+}
